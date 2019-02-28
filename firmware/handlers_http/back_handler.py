9168d522d4f5aab3d4c153a0d9d75e36df54672c
Martin Rosas <martinrosas84@gmail.com>
2019-02-28 15:40:18 -0300

    backs correction

--- firmware/handlers_http/back_handler.py
+++ firmware/handlers_http/back_handler.py
@@ -2,7 +2,7 @@ from handlers_http.basic_handler import BasicHandler
 
 class BackSelectCalibrationHandler(BasicHandler):
     def get(self):
-        #self.firmware.homming()
+        self.firmware.homming()
         self.render("select_calibration.html")
 
 class BackLoadUnloadHandler(BasicHandler):
