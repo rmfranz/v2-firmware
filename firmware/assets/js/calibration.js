42a9210f12fdf070c0f2e28868c7b21528216bb9
Martin Rosas <martinrosas84@gmail.com>
2019-03-13 10:10:09 -0300

    fix close popup

--- firmware/assets/js/calibration.js
+++ firmware/assets/js/calibration.js
@@ -219,3 +219,11 @@ $("#help_xy_calibration").on("click", function() {
 $("#help_z_calibration").on("click", function() {
     $("#help_calibration_modal").toggleClass( "k-modal-overlay--visible" );
 });
+
+$("#help_calibration_modal_close").on("click", function() {
+    $("#help_xy_calibration").toggleClass( "k-modal-overlay--visible" );
+});
+
+$("#help_calibration_modal_close").on("click", function() {
+    $("#help_z_calibration").toggleClass( "k-modal-overlay--visible" );
+});
