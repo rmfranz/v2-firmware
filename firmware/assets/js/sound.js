7143b40cc3975df0d5fd5ec964986b7ab1e9c0bf
Martin Rosas <martinrosas84@gmail.com>
2019-03-13 17:47:00 -0300

    sound functionality

--- firmware/assets/js/sound.js
+++ firmware/assets/js/sound.js
@@ -1,6 +1,7 @@
 $( "#sound" ).change(function() {
     $.post( "/sound", { volume: $(this).val()})
     .done(function( data ) {
-        $("volume").text(data);
+        document.getElementById("audio").play();
+        $("#volume").text(data);
     });
 });
