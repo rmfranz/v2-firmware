038370b349d8a7dc793d7098ce22c2fbd5909b5d
Martin Rosas <martinrosas84@gmail.com>
2019-03-18 15:23:13 -0300

    change 25 calibration

--- firmware/firmware/smoothie_firmware.py
+++ firmware/firmware/smoothie_firmware.py
@@ -249,10 +249,12 @@ class SmoothieFirmware(BaseFirmware):
     def calibration_25_points_until_complete(self):
         self.printrun.send_now("G28")
         self.printrun.send_now("G32")
-
-    def calibration_25_points_for_complete(self):
         self.printrun.send_now("M374")
         self.printrun.send_now("M500")
+
+    def calibration_25_points_for_complete(self):
+        #self.printrun.send_now("M374")
+        #self.printrun.send_now("M500")
         self.printrun.send_now("G28")
 
     def show_grid_25_points(self):
