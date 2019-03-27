4149f34a1b5ecfe6abd9bb8bead4dfe0528d281d
Martin Rosas <martinrosas84@gmail.com>
2019-03-27 15:45:54 -0300

    fix async cloud

--- firmware/assets/js/cloud_registration.js
+++ firmware/assets/js/cloud_registration.js
@@ -1,5 +1,5 @@
 $.get("/get-cloud-token").done(function (data) {
-    $('#registration_code').text(data.registration_code);
+    $('#registration_code_btn').text(data.registration_code);
     $("#waiting_info").toggleClass("initiallyHidden");
     $("#registration_code").toggleClass("initiallyHidden");
 });
