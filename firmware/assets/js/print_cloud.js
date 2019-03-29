72c1a7ff94925a487254c21ce68519480afb462a
Martin Rosas <martinrosas84@gmail.com>
2019-03-29 17:50:18 -0300

    change resp to dict

--- firmware/assets/js/print_cloud.js
+++ firmware/assets/js/print_cloud.js
@@ -1,9 +1,10 @@
 
 $.get("/get-cloud-queue").done(function (data) {
+    var resp = data.resp
     var select = $("#cloud_files");
-    for (var i = 0; i < data.length; i++) {
-        var d = '<div class="k-block-2 file_selected" data-job_id="' + data[i].id + '" data-filename="' + data[i].filename + '">'
-        d += '<div class="k-block-2__left"><div class="k-block-2__img-container"> </div> <p>' + data[i].filename + '</p> </div> </div>'
+    for (var i = 0; i < resp.length; i++) {
+        var d = '<div class="k-block-2 file_selected" data-job_id="' + resp[i].id + '" data-filename="' + resp[i].filename + '">'
+        d += '<div class="k-block-2__left"><div class="k-block-2__img-container"> </div> <p>' + resp[i].filename + '</p> </div> </div>'
         select.append(d);
     }
 }
