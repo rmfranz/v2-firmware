b0bdc642333baadfc34e2ed2576a4a79a6f72b20
Martin Rosas <martinrosas84@gmail.com>
2019-04-30 16:29:31 -0300

    g28 on back 3pt calibration

--- firmware/firmware/smoothie_firmware_3pt.py
+++ firmware/firmware/smoothie_firmware_3pt.py
@@ -8,7 +8,7 @@ class SmoothieFirmware3PT(SmoothieFirmware):
         self.printrun.send_now("G1 Z5 F4000")
         self.printrun.send_now("G32")
         self.printrun.send_now("M500")
-        self.printrun.send_now("G28")
+        #self.printrun.send_now("G28")
 
     def give_calibration_page(self):
         return "3_calibration.html"
