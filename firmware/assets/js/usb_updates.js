b0712adab30ea073b92741ec5114e624c6f022ff
Martin Rosas <martinrosas84@gmail.com>
2019-05-23 18:38:26 -0300

    popup on update process

--- firmware/assets/js/usb_updates.js
+++ firmware/assets/js/usb_updates.js
@@ -1,16 +1,22 @@
 
 $("#update").on("click", function () {
+    $('#updateModal').toggleClass("k-modal-overlay--visible");
     $.get("/make-usb-update").done(function(data){
         if(data.error == 1) {
             $('#mount_error').toggleClass("initiallyHidden");
+            $('#updateModal').toggleClass("k-modal-overlay--visible");
         } else if(data.error == 2) {
             $('#unmount_error').toggleClass("initiallyHidden");
+            $('#updateModal').toggleClass("k-modal-overlay--visible");
         } else if(data.error == 3) {
             $('#download_error').toggleClass("initiallyHidden");
+            $('#updateModal').toggleClass("k-modal-overlay--visible");
         } else if(data.error == 4) {
             $('#copy_unzip_error').toggleClass("initiallyHidden");
+            $('#updateModal').toggleClass("k-modal-overlay--visible");
         } else if(data.error == 5) {
             $('#folder_error').toggleClass("initiallyHidden");
+            $('#updateModal').toggleClass("k-modal-overlay--visible");
         }
     })
 })
