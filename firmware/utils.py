00b3983d294dca58aeea464f7b22fb1915c7c7dc
Martin Rosas <martinrosas84@gmail.com>
2019-05-23 18:25:50 -0300

    catch when usb is gone

--- firmware/utils.py
+++ firmware/utils.py
@@ -62,12 +62,15 @@ def mount_usb(uuid_board):
         if result != 0:
             return 1
     #TODO: Check if more than one
-    usb_info = check_output("ls -l /dev/disk/by-uuid | grep sd | grep -v {}".format(uuid_board),
-                    shell=True, universal_newlines=True)
-    #a = check_output("ls -l /dev/disk/by-uuid | grep sd", shell=True, universal_newlines=True)
-    usb_info = [j for j in usb_info.split("\n") if j]
-    #usb = {usb_info[0].split()[8]: usb_info[0].split()[10].split("/")[-1]}
-    return os.system("sudo mount /dev/{} /media/usb -o uid=pi,gid=pi".format(usb_info[0].split()[10].split("/")[-1]))
+    try:
+        usb_info = check_output("ls -l /dev/disk/by-uuid | grep sd | grep -v {}".format(uuid_board),
+                        shell=True, universal_newlines=True)
+        #a = check_output("ls -l /dev/disk/by-uuid | grep sd", shell=True, universal_newlines=True)
+        usb_info = [j for j in usb_info.split("\n") if j]
+        #usb = {usb_info[0].split()[8]: usb_info[0].split()[10].split("/")[-1]}
+        return os.system("sudo mount /dev/{} /media/usb -o uid=pi,gid=pi".format(usb_info[0].split()[10].split("/")[-1]))
+    except:
+        return 2
 
 def get_gcodes_from_usb():
     dirName = "/media/usb"
