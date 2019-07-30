c6c1e7b1936645a2b0a43993020bf797e3d399bd
Martin Rosas <martinrosas84@gmail.com>
2019-07-30 11:49:57 -0300

    change the way to form html

--- firmware/utils.py
+++ firmware/utils.py
@@ -276,17 +276,14 @@ def filter_gcode_file(path, name):
 
 def path_to_html(path):
     pattern = "*.gcode"
-    html = '<details class="k-block-2"><summary class="k-block-2__left k-block-2__left--directory">'
+    html = ''
     for item in sorted(os.listdir(path)):
         fullpath = os.path.join(path, item)
         if os.path.isdir(fullpath) and not item.startswith("."):
-            #html += '<summary class="k-block-2__left k-block-2__left--directory"><p>%s</p>' % item
-            #html += path_to_html(fullpath) + '</summary>'
-            html += '<p>%s</p>' % item
-            html += path_to_html(fullpath)
+            html += '<details class="k-block-2"><summary class="k-block-2__left k-block-2__left--directory"><p>%s</p></summary>' % item
+            html += path_to_html(fullpath) + '</details>'
         elif fnmatch.fnmatch(fullpath, pattern) and not item.startswith("."):
             html += '<div class="k-block-2 file_selected" data-path="{}" data-filename="{}"><div class="k-block-2__left"><p>{}</p></div></div>'.format(fullpath, item, item)
-    html += '</summary></details>'
     return html
 
 def return_wlan_info():
